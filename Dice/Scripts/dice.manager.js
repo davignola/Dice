ae6ac5abdc1d07b8dfadf46d4083369485f3aec1
Denis-Andre Roussy-Vignola <denis.andre.vignola@gmail.com>
2016-07-18 02:45:14 -0400

    Much progress !

--- Dice/Scripts/dice.manager.js
+++ Dice/Scripts/dice.manager.js
@@ -5,15 +5,33 @@ if (!dice) {
 dice.manager = new function () {
     var self = this;
     this.players = [];
+    this.currentPlayer = null;
+    this.targetScore = 10000;
+    // Game ends when a player matches this id
+    this.lastRoundPlayerId = -1;
+    this.roundCount = 0;
+
+
     this.addPlayer = function (name) {
         var newIndex = self.getMaxIndex() + 1;
         name = name || "Joueur " + (newIndex + 1);
-
-        self.players.push(new dice.component.Player(name, newIndex));
+        var newPlayer = new dice.component.Player(name, newIndex);
+        self.players.push(newPlayer);
+        dice.ui.addPlayerContainer(newPlayer);
     }
 
     this.getMaxIndex = function () {
         var result = Math.max.apply(Math, self.players.map(function (o) { return o.index; }));
         return result === -Infinity ? -1 : result;
+    }
+
+    this.nextPlayer = function (playerIndex) {
+        playerIndex = playerIndex || (self.currentPlayer ? self.currentPlayer.index + 1 : 0);
+        if (playerIndex > self.getMaxIndex()) {
+            playerIndex = 0;
+            self.roundCount++;
+        }
+        self.currentPlayer = self.players[playerIndex];
+        dice.ui.setCurentPlayer(playerIndex);
     }
 }
--- Dice/Scripts/dice.ui.js
+++ Dice/Scripts/dice.ui.js
@@ -2,16 +2,70 @@
 if (!dice) {
     var dice = {};
 }
-dice.ui = new function() {
+dice.ui = new function () {
     var self = this;
 
     var diceView = {
-        playerContainerId: "dice-scoreboard"
+        playerSlots: 12,
+        playerContainerId: "dice-scoreboard",
+        playerColumnAttribute: "data-player-container",
+        playerPanelAttribute: "data-player-panel",
+        playerPanelTitleBaseId: "dice-player-name-",
+        playerPanelFooterBaseId: "dice-player-total-",
+        playerScoreBoardBaseId: "dice-player-scoreboard-",
+        previousColumnSize: 0
     };
 
-    this.addPlayerContainer = function() {
+    this.addPlayerContainer = function (playerObject) {
+        /// <summary>Add the ui column player container</summary>
+        /// <param name="playerObject" type="dice.components.Player"></param>
+        // Get main container
         var scoreboard = $("#" + diceView.playerContainerId);
+        // This is the column
         var playerContainer = $("<div>");
-        playerContainer.addClass(c)
+        playerContainer.attr(diceView.playerColumnAttribute, playerObject.index);
+        // Panel with title and scoreboard
+        var playerPanel = $("<div>");
+        playerPanel
+            .addClass("panel panel-default")
+            .attr(diceView.playerPanelAttribute, playerObject.index)
+            .append($("<div>").addClass("panel-heading")
+            // Adding title
+                            .append($("<h3>").addClass("panel-title")
+                                             .attr("id", diceView.playerPanelTitleBaseId + playerObject.index)
+                                             .text(playerObject.name)))
+            // Adding body (scoreboard)
+            .append($("<div>").addClass("panel-body")
+                            .attr(diceView.playerScoreBoardBaseId + playerObject.index))
+            // Total in footer
+            .append($("<div>").addClass("panel-footer")
+                                .append($("<h3>")
+                                .attr("id", diceView.playerPanelFooterBaseId + playerObject.index)
+                                .text("Total: 0")));
+        // Adding to containers
+        playerContainer.append(playerPanel);
+        scoreboard.append(playerContainer);
+        // update columns classes
+        self.setColumnClass();
+    }
+
+    this.setColumnClass = function () {
+        var playerCount = dice.manager.players.length;
+        var containers = $("#" + diceView.playerContainerId).find("div[" + diceView.playerColumnAttribute + "]");
+        if (diceView.previousColumnSize > 0) {
+            containers.removeClass("col-md-" + diceView.previousColumnSize);
+        }
+        var columnSize = Math.max(Math.floor(diceView.playerSlots / playerCount), 1);
+        containers.addClass("col-md-" + columnSize);
+        diceView.previousColumnSize = columnSize;
+    }
+
+    this.setCurentPlayer = function (playerIndex) {
+        $("div[" + diceView.playerPanelAttribute + "]")
+            .removeClass("panel-primary")
+            .addClass("panel-default");
+        $("div[" + diceView.playerPanelAttribute + "=" + playerIndex + "]")
+            .removeClass("panel-default")
+            .addClass("panel-primary");
     }
 }
